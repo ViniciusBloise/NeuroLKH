16a76b2fc2a0cc292682b37c7d871075288f6c6d
yzhang <yzhang@d1.com>
2022-05-24 14:45:42 +0800

    ...

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
+set -e
 cd SRC_swig
 swig -python LKH.i
 python setup.py build_ext -i
 # use `-i` (`--inplace`) and the following commands are not needed any more.
-# https://www.swig.org/Doc1.3/Python.html#Python_nn6
-# #####
+# see https://www.swig.org/Doc1.3/Python.html#Python_nn6
+
 # cd ..
 # cp SRC_swig/build/lib.*/_LKH.*.so ./
