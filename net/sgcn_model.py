004f82130135d8fd5aebfd49b3c0ae45518fca71
ViniciusBloise <vinicius.bloise@gmail.com>
2022-07-01 07:21:07 +0200

    Experiment to see NeuroLKH running Reformat

--- net/sgcn_model.py
+++ net/sgcn_model.py
@@ -4,22 +4,24 @@ import torch.nn as nn
 
 from .sgcn_layers import SparseGCNLayer, MLP
 
+
 def loss_edges(y_pred_edges, y_edges, edge_cw):
     y_pred_edges = y_pred_edges.permute(0, 2, 1)  # batch_size x 2 x n_node * n_edge
     loss_edges = nn.NLLLoss(edge_cw, reduction="none")(y_pred_edges, y_edges)
     return loss_edges
 
+
 class SparseGCNModel(nn.Module):
     def __init__(self, hidden_dim=128, n_gcn_layers=30, n_mlp_layers=3, problem="tsp"):
         super(SparseGCNModel, self).__init__()
         if problem == "tsp":
-            self.node_dim = 2 # x, y
+            self.node_dim = 2  # x, y
         elif problem == "cvrp":
-            self.node_dim = 4 # x, y, demand, capacity
+            self.node_dim = 4  # x, y, demand, capacity
         elif problem == "pdp":
-            self.node_dim = 5 # x, y, depot, pickup, delivery
+            self.node_dim = 5  # x, y, depot, pickup, delivery
         elif problem == "cvrptw":
-            self.node_dim = 6 # x, y, demand, start_time, end_time, capacity
+            self.node_dim = 6  # x, y, demand, start_time, end_time, capacity
         else:
             assert False
         self.edge_dim = 1
@@ -33,7 +35,7 @@ class SparseGCNModel(nn.Module):
         self.edges_embedding = nn.Linear(self.edge_dim, self.hidden_dim, bias=False)
         gcn_layers = []
         for layer in range(self.n_gcn_layers):
-            gcn_layers.append(SparseGCNLayer(self.hidden_dim, self.aggregation, is_pdp=problem=="pdp"))
+            gcn_layers.append(SparseGCNLayer(self.hidden_dim, self.aggregation, is_pdp=problem == "pdp"))
         self.gcn_layers = nn.ModuleList(gcn_layers)
         if problem == "pdp" or problem == "cvrptw":
             self.mlp_edges = MLP(self.hidden_dim, 2, self.n_mlp_layers)
@@ -52,9 +54,9 @@ class SparseGCNModel(nn.Module):
         y_pred_edges = self.mlp_edges(e).view(batch_size, num_nodes, n_edges)
         y_pred_edges = torch.exp(y_pred_edges)
         y_pred_edges = y_pred_edges / (y_pred_edges.sum(2).view(batch_size, num_nodes, 1) + 1e-5)
-        
+
         y_pred_edges = y_pred_edges.view(batch_size, num_nodes * n_edges, 1)
-        y_pred_edges = torch.cat([1 - y_pred_edges, y_pred_edges], dim = 2)
+        y_pred_edges = torch.cat([1 - y_pred_edges, y_pred_edges], dim=2)
         y_pred_edges = torch.log(y_pred_edges)
         if y_edges != None:
             loss = loss_edges(y_pred_edges, y_edges, edge_cw)
@@ -85,7 +87,7 @@ class SparseGCNModel(nn.Module):
         if self.problem == "pdp":
             node_state = torch.zeros((batch_size, num_nodes, 3), device=x_nodes.device)
             node_state[:, 0, 0] = 1
-            node_state[:, 1 : num_nodes // 2 + 1, 1] = 1
+            node_state[:, 1: num_nodes // 2 + 1, 1] = 1
             node_state[:, num_nodes // 2 + 1:, 2] = 1
             x_nodes = torch.cat([x_nodes, node_state], 2)
 
@@ -100,12 +102,12 @@ class SparseGCNModel(nn.Module):
         y_pred_edges1 = torch.exp(y_pred_edges[:, :, :, 0])
         y_pred_edges1 = y_pred_edges1 / (y_pred_edges1.sum(2).view(batch_size, num_nodes, 1) + 1e-5)
         y_pred_edges1 = y_pred_edges1.view(batch_size, num_nodes * n_edges, 1)
-        y_pred_edges1 = torch.cat([1 - y_pred_edges1, y_pred_edges1], dim = 2)
+        y_pred_edges1 = torch.cat([1 - y_pred_edges1, y_pred_edges1], dim=2)
         y_pred_edges1 = torch.log(y_pred_edges1)
         y_pred_edges2 = torch.exp(y_pred_edges[:, :, :, 1])
         y_pred_edges2 = y_pred_edges2 / (y_pred_edges2.sum(2).view(batch_size, num_nodes, 1) + 1e-5)
         y_pred_edges2 = y_pred_edges2.view(batch_size, num_nodes * n_edges, 1)
-        y_pred_edges2 = torch.cat([1 - y_pred_edges2, y_pred_edges2], dim = 2)
+        y_pred_edges2 = torch.cat([1 - y_pred_edges2, y_pred_edges2], dim=2)
         y_pred_edges2 = torch.log(y_pred_edges2)
         if y_edges1 is None:
             return y_pred_edges1, y_pred_edges2, None, None, None
