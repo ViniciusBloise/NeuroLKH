66dec6432858f0e5620f747d89de5eb27b470266
ViniciusBloise <vinicius.bloise@gmail.com>
2022-07-06 07:08:56 +0200

    prepare to call LKH solver

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from intf.readerTSP import ReaderTSP
 from intf.plotterTSP import PlotterTSP
 from intf.config import Config
 from intf.graph_builder import GraphBuilder
+from intf.lkh_helper import solve_NeuroLKH
 import networkx as nx
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 config = Config(tspInstUsed=os.listdir(r'./result/1000/tsp'))
@@ -21,6 +23,8 @@ print(positions)
 plotter = PlotterTSP(None)
 G = GraphBuilder(pos=positions, dist_matrix=distance_matrix)
 G.add_minimum_spanning_tree()
-plt.show()
+#plt.show()
+
+solve_NeuroLKH(dataset_name='1000', instance=None, instance_name=name, candidate=np.empty(0), pi=np.empty(0), rerun=True)
 
 input('Hit <ENTER> to end.')
